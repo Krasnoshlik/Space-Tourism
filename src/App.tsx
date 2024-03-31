25e024c60c54a8d3f4c49f0e0e9399e1a7d34a80
Krasnoshlik <dimakrasnoshluk@gmail.com>
2024-03-31 17:54:43 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -66,16 +66,6 @@ function App() {
                 <button onClick={() => navigate('/', { replace: false })}>00 HOME</button>
                 <button onClick={() => navigate('/destination', { replace: false })}>01 DESTINATION</button>
 
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
-              <g fill="none" fill-rule="evenodd">
-                <circle cx="24" cy="24" r="24" fill="#FFF" />
-                <path
-                  fill="#0B0D17"
-                  d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-                />
-              </g>
-            </svg>
-
                 <button onClick={() => navigate('/crew', { replace: false })}>02 CREW</button>
                 <button onClick={() => navigate('/technology', { replace: false })}>03 TECHNOLOGY</button>
               </div>
